fe9845307c510a52e8bc90b9333ad197a524feae
Julio Cezar Gorgueira <gorgueirajc@gmail.com>
2026-02-19 23:41:48 -0300

    Delete Jvips/src/main directory

--- Jvips/src/main/java/br/com/julio/jvips/core/CommandDispatcher.java
+++
@@ -1,8 +0,0 @@
-package br.com.julio.jvips.core;
-
-import java.util.concurrent.CompletableFuture;
-
-@FunctionalInterface
-public interface CommandDispatcher {
-    CompletableFuture<Void> dispatchAsConsole(String rawCmd);
-}
